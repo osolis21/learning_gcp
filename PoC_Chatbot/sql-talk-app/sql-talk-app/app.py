23fe85f19ad5022ae5220c80c311afb99a9f85d2
osolis21 <osolis@deacero.com>
2024-04-18 11:28:29 -0600

    Tiene problemas al leer las tablas

---
+++ PoC_Chatbot/sql-talk-app/sql-talk-app/app.py
@@ -0,0 +1,280 @@
+import time
+
+from google.cloud import bigquery
+import streamlit as st
+from vertexai.generative_models import FunctionDeclaration, GenerativeModel, Part, Tool
+
+BIGQUERY_DATASET_ID = "poc-test-omsg.poc_wtp"
+
+list_datasets_func = FunctionDeclaration(
+    name="list_datasets",
+    description="Get a list of datasets that will help answer the user's question",
+    parameters={
+        "type": "object",
+        "properties": {},
+    },
+)
+
+list_tables_func = FunctionDeclaration(
+    name="list_tables",
+    description="List tables in a dataset that will help answer the user's question",
+    parameters={
+        "type": "object",
+        "properties": {
+            "dataset_id": {
+                "type": "string",
+                "description": "Dataset ID to fetch tables from.",
+            }
+        },
+        "required": [
+            "dataset_id",
+        ],
+    },
+)
+
+get_table_func = FunctionDeclaration(
+    name="get_table",
+    description="Get information about a table, including the description, schema, and number of rows and columns that will help answer the user's question. Always use the fully qualified dataset and table names.",
+    parameters={
+        "type": "object",
+        "properties": {
+            "table_id": {
+                "type": "string",
+                "description": "Fully qualified ID of the table to get information about",
+            }
+        },
+        "required": [
+            "table_id",
+        ],
+    },
+)
+
+sql_query_func = FunctionDeclaration(
+    name="sql_query",
+    description="Get information from data in BigQuery using SQL queries",
+    parameters={
+        "type": "object",
+        "properties": {
+            "query": {
+                "type": "string",
+                "description": "SQL querycd  that will help give quantitative answers to the user's question when run on a BigQuery dataset poc-test-omsg.poc_wtp and tables poc-test-omsg.poc_wtp.table_poc_wtp or poc-test-omsg.poc_wtp.table_poc_wtp_sample. In the SQL query, always use the fully qualified dataset and table names.",
+            }
+        },
+        "required": [
+            "query",
+        ],
+    },
+)
+
+sql_query_tool = Tool(
+    function_declarations=[
+        list_datasets_func,
+        list_tables_func,
+        get_table_func,
+        sql_query_func,
+    ],
+)
+
+model = GenerativeModel(
+    "gemini-1.0-pro",
+    generation_config={"temperature": 0},
+     tools=[sql_query_tool],
+)
+
+st.set_page_config(
+    page_title="PoC WTP-HTW",
+    page_icon="vertex-ai.png",
+    layout="wide",
+)
+
+col1, col2 = st.columns([8, 1])
+with col1:
+    st.title("PoC WTP-HTW")
+with col2:
+    st.image("vertex-ai.png")
+
+st.subheader("Powered by Function Calling in Gemini")
+
+#st.markdown(
+#    "[Source Code](https://github.com/GoogleCloudPlatform/generative-ai/tree/main/gemini/function-calling/sql-talk-app/)   •   [Documentation](https://cloud.google.com/vertex-ai/docs/generative-ai/multimodal/function-calling)   •   [Codelab](https://codelabs.developers.google.com/codelabs/gemini-function-calling)   •   [Sample Notebook](https://github.com/GoogleCloudPlatform/generative-ai/blob/main/gemini/function-calling/intro_function_calling.ipynb)"
+#)
+
+with st.expander("Sample prompts", expanded=True):
+    st.write(
+        """
+        - Qué información hay en el dataset?
+
+        
+    """
+    )
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"].replace("$", "\$"))  # noqa: W605
+        try:
+            with st.expander("Function calls, parameters, and responses"):
+                st.markdown(message["backend_details"])
+        except KeyError:
+            pass
+
+if prompt := st.chat_input("Ask me about information in the database...."):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        chat = model.start_chat()
+        client = bigquery.Client()
+
+        prompt += """
+            You are an data engineer expert using bigquery, and you need to give useful insights to management. 
+            You will receive prompts in spanish. 
+            The information, schema and descriptions in the tables you will read is also in spanish. 
+            Please give a concise, high-level summary followed by detail in
+            plain language about where the information in your response is
+            coming from in the database. Only use information that you learn
+            from BigQuery dataset poc-test-omsg.poc_wtp, which contains tables:poc-test-omsg.poc_wtp.table_poc_wtp and poc-test-omsg.poc_wtp.table_poc_wtp_sample .
+            Do not make up information, do not consult big query public datasets. Answer in spanish. 
+            If you are asked information regarding client name, state name, city name or product name, use "like" in the query.
+            Does not make up table names or column names. 
+            If you can't find a column label related to the question in the promp, say so and ask for a more specific name. 
+            Remember that:
+            NombreCliente contains the client name
+            Nombre Estado cointains the state where the purchase order was made
+            Nombre Producto contains the name of the product purchased
+            ImpPrecioListaMN constains the product price 
+            """
+
+        response = chat.send_message(prompt)
+        response = response.candidates[0].content.parts[0]
+
+        print(response)
+
+        api_requests_and_responses = []
+        backend_details = ""
+
+        function_calling_in_process = True
+        while function_calling_in_process:
+            try:
+                params = {}
+                for key, value in response.function_call.args.items():
+                    params[key] = value
+
+                print(response.function_call.name)
+                print(params)
+
+                if response.function_call.name == "list_datasets":
+                    api_response = client.list_datasets()
+                    api_response = BIGQUERY_DATASET_ID
+                    api_requests_and_responses.append(
+                        [response.function_call.name, params, api_response]
+                    )
+
+                if response.function_call.name == "list_tables":
+                    api_response = client.list_tables(params["dataset_id"])
+                    api_response = str([table.table_id for table in api_response])
+                    api_requests_and_responses.append(
+                        [response.function_call.name, params, api_response]
+                    )
+
+                if response.function_call.name == "get_table":
+                    api_response = client.get_table(params["table_id"])
+                    api_response = api_response.to_api_repr()
+                    api_requests_and_responses.append(
+                        [
+                            response.function_call.name,
+                            params,
+                            [
+                                str(api_response.get("description", "")),
+                                str(
+                                    [
+                                        column["name"]
+                                        for column in api_response["schema"]["fields"]
+                                    ]
+                                ),
+                            ],
+                        ]
+                    )
+                    api_response = str(api_response)
+
+                if response.function_call.name == "sql_query":
+                    job_config = bigquery.QueryJobConfig(
+                        maximum_bytes_billed=100000000
+                    )  # Data limit per query job
+                    try:
+                        cleaned_query = (
+                            params["query"]
+                            .replace("\\n", " ")
+                            .replace("\n", "")
+                            .replace("\\", "")
+                        )
+                        query_job = client.query(cleaned_query, job_config=job_config)
+                        api_response = query_job.result()
+                        api_response = str([dict(row) for row in api_response])
+                        api_response = api_response.replace("\\", "").replace("\n", "")
+                        api_requests_and_responses.append(
+                            [response.function_call.name, params, api_response]
+                        )
+                    except Exception as e:
+                        api_response = f"{str(e)}"
+                        api_requests_and_responses.append(
+                            [response.function_call.name, params, api_response]
+                        )
+
+                print(api_response)
+
+                response = chat.send_message(
+                    Part.from_function_response(
+                        name=response.function_call.name,
+                        response={
+                            "content": api_response,
+                        },
+                    ),
+                )
+                response = response.candidates[0].content.parts[0]
+
+                backend_details += "- Function call:\n"
+                backend_details += (
+                    "   - Function name: ```"
+                    + str(api_requests_and_responses[-1][0])
+                    + "```"
+                )
+                backend_details += "\n\n"
+                backend_details += (
+                    "   - Function parameters: ```"
+                    + str(api_requests_and_responses[-1][1])
+                    + "```"
+                )
+                backend_details += "\n\n"
+                backend_details += (
+                    "   - API response: ```"
+                    + str(api_requests_and_responses[-1][2])
+                    + "```"
+                )
+                backend_details += "\n\n"
+                with message_placeholder.container():
+                    st.markdown(backend_details)
+
+            except AttributeError:
+                function_calling_in_process = False
+
+        time.sleep(3)
+
+        full_response = response.text
+        with message_placeholder.container():
+            st.markdown(full_response.replace("$", "\$"))  # noqa: W605
+            with st.expander("Function calls, parameters, and responses:"):
+                st.markdown(backend_details)
+
+        st.session_state.messages.append(
+            {
+                "role": "assistant",
+                "content": full_response,
+                "backend_details": backend_details,
+            }
+        )
